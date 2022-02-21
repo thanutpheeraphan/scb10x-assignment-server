d9d369d7330776814df4953eab513f58515280b0
Thanut Pheeraphan <thanut@wongnai.com>
2022-02-21 08:49:58 +0700

    added fix in index and scripts

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use("/auth", require("./routes/auth"));
 
 app.use("/room", require("./routes/room"));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+	app.get("*", (req, res) => {
+	  res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+	});
+  }
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
