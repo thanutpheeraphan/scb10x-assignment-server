49f09a024c88163f229c6bad04866eafe549bb7b
Thanut Pheeraphan <thanut@wongnai.com>
2022-02-21 10:10:58 +0700

    changed location of ssl

--- db.js
+++ db.js
@@ -1,23 +1,20 @@
-const Pool = require("pg").Pool;
+const Pool = require("pg").Pool
 
 const devConfig = new Pool({
-  user: "postgres",
-  password: "admin",
-  host: "localhost",
-  port: 5432,
-  database: "partyweb",
+	user: "postgres",
+	password: "admin",
+	host: "localhost",
+	port: 5432,
+	database: "partyweb"
 });
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig,
-  {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }
-);
+const proConfig = {
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+        rejectUnauthorized: false,
+    }
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig );
 module.exports = pool;
